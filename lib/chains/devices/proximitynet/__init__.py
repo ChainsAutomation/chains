94dbcdf33745bc25c70d83859d2467aaab018242
ChrisQ <chrisq@chainsautomation.com>
2015-04-16 16:44:49 +0200

    Proximity by ARP on network device

---
+++ lib/chains/devices/proximitynet/__init__.py
@@ -0,0 +1,20 @@
+import chains.device
+from chains.common import log
+import datetime
+import scapy.all import *
+
+class ProximitynetDevice(chains.device.Device):
+
+    def onStart(self):
+        last = {}
+        while not self._shutdown:
+            sniff(prn=self.arp_monitor_callback, filter="arp", store=0)
+
+    def arp_monitor_callback(self, pkt):
+        if pkt[ARP].op == 1:  # who-has (request)
+            t = datetime.datetime.now()
+            log.info("ARP Request: " + pkt[ARP].psrc + " is asking about " + pkt[ARP].pdst)
+            self.sendEvent(pkt.psrc, {'time': t})
+            return pkt.psrc
+        if pkt[ARP].op == 2:  # is-at (response)
+            log.info("ARP Response: " + pkt[ARP].hwsrc + " has address " + pkt[ARP].psrc)
