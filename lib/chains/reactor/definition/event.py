b4843a71eab30832f430ce8c616ebc14b8bd1362
Stian Strandem <stian@eonbit.com>
2015-05-11 20:40:17 +0200

    Improved event matching and wrote test for it

--- lib/chains/reactor/definition/event.py
+++ lib/chains/reactor/definition/event.py
@@ -1,46 +1,89 @@
 import types
 
-# todo: test
 # todo: how to match data
 class Event:
 
-    def __init__(self, **kw):
-        for key in kw:
-            setattr(self, key, kw[key])
+    def __init__(self, device='*', key='*', data='*'):
+        self.device = device
+        self.key    = key
+        self.data   = data
 
-    # quick'n'dirty matching for testing
-    # probably needs some l0ve to work in production
-
-    # Match one of the passed events
-    def match(self, event): # event can be 1 Event or list of events
+    # Check if this event is matched by "event" passed to this function
+    # 
+    # Be aware that what is "this" event and what is "event" does matter!
+    # 
+    # This event (ie. self) is the occuring event, and "event" passed to the function
+    # is the event configured in the rule. That means "self" may have more data than
+    # "event" and still match, but not vice versa. For details see _matchDict() below.
+    #
+    # Also note that "event" may be a list of event, in which case _one_ of them must
+    # match (ie. OR operation). While "self" can (obviously) only be a single event.
+    #
+    # TL;DR: e1.match(e2) and e2.match(e1) may not give the same result!
+    #
+    def match(self, event):
         if type(event) == types.ListType:
             events = event
         else:
             events = [event]
         for event2 in events:
-            if self.matchEvent(event2):
+            if self._matchEvent(event2):
                 return True
         return False
         
     # Match a single event
-    def matchEvent(self, event):
+    def _matchEvent(self, event):
         for key in dir(self):
             if key[0] == '_': 
                 continue
-            val1 = self.tryattr(self, key)
-            val2 = self.tryattr(event, key)
+            val1 = self._tryattr(self, key)
+            val2 = self._tryattr(event, key)
             if type(val1) == types.MethodType or type(val2) == types.MethodType:
                 continue
-            if not self.matchValues(val1,val2):
+            if not self._matchValues(val1,val2):
                 return False
         return True
 
-    def matchValues(self, val1, val2):
-        if val1 == val2: return True
-        if val1 == '*' or val2 == '*': return True
+    def _matchValues(self, val1, val2):
+        if val1 == val2: 
+            return True
+        if val1 == '*' or val2 == '*': 
+            return True
+        if type(val1) == types.DictType and type(val2) == types.DictType:
+            return self._matchDicts(val1, val2)
         return False
 
-    def tryattr(self, obj, key):
+    
+    # d1 is this event (self), which is the occurring event
+    # d2 is the event in the rule
+    #
+    # That means _all_ of d2's items must match d1's items,
+    # but not all of d1's items must match d2's items.
+    #
+    # F.ex: The rule says:
+    #   yield Event(device='foo', data={'value': 2})
+    #
+    # That should match this occuring event:
+    #   Event(device='foo', data={'value': 2, 'type': 'humidity'})
+    #
+    # But if the rule says:
+    #   yield Event(device='foo', data={'value': 2, 'type': 'humidity'})
+    #
+    # That should NOT match this occurring event:
+    #   Event(device='foo', data={'value': 2})
+    #
+    def _matchDicts(self, d1, d2):
+        for key in d2:
+            v2 = d2[key]
+            try:
+                v1 = d1[key]
+            except KeyError:
+                return False
+            if not self._matchValues(v1, v2):
+                return False
+        return True
+
+    def _tryattr(self, obj, key):
         try: return getattr(obj, key)
         except AttributeError: return None
 
---
+++ test/reactor/test_event.py
@@ -0,0 +1,158 @@
+import unittest
+from chains.reactor.definition.event import Event
+from chains.common import log
+#import rule1, rule2, rule3
+#import time
+
+class TestEvent(unittest.TestCase):
+
+    def setUp(self):
+        log.setLevel('debug')
+
+    # Instantiation
+
+    def test_When_passing_valid_kwargs_They_are_set(self):
+        e = Event(device='dev', key='key', data='data')
+        self.assertEqual(e.device, 'dev')
+        self.assertEqual(e.key,    'key')
+        self.assertEqual(e.data,   'data')
+
+    def test_When_passing_valid_positional_args_They_are_set(self):
+        e = Event('dev', 'key', 'data')
+        self.assertEqual(e.device, 'dev')
+        self.assertEqual(e.key,    'key')
+        self.assertEqual(e.data,   'data')
+
+    def test_When_passing_invalid_kwargs_TypeError_is_raised(self):
+        try:
+            e = Event(foo='invalid')
+        except TypeError:
+            return
+        raise Exception('Expected TypeError, got none')
+
+    def test_When_leaving_all_args_out_They_are_set_to_wildcard(self):
+        e = Event()
+        self.assertEqual(e.device, '*')
+        self.assertEqual(e.key,    '*')
+        self.assertEqual(e.data,   '*')
+
+    def test_When_leaving_some_args_out_They_are_set_to_wildcard(self):
+        e = Event(key='foo')
+        self.assertEqual(e.device, '*')
+        self.assertEqual(e.key,    'foo')
+        self.assertEqual(e.data,   '*')
+
+    # Matching
+
+    def test_When_matching_exact_correct_values_They_are_matched(self):
+        e1 = Event(device='dev', key='key', data='data')
+        e2 = Event(device='dev', key='key', data='data')
+        self.assertTrue(e1.match(e2))
+
+    def test_When_matching_exact_incorrect_values_They_are_not_matched(self):
+        e1 = Event(device='dev', key='key', data='data')
+        e2 = Event(device='dev1', key='key1', data='data1')
+        self.assertFalse(e1.match(e2))
+        e2 = Event(device='dev1', key='key', data='data')
+        self.assertFalse(e1.match(e2))
+        e2 = Event(device='dev', key='key1', data='data')
+        self.assertFalse(e1.match(e2))
+        e2 = Event(device='dev', key='key', data='data1')
+        self.assertFalse(e1.match(e2))
+
+    def test_When_matching_wildcards_They_are_matched(self):
+        e1 = Event(device='dev', key='*', data='*')
+        e2 = Event(device='dev', key='key', data={'value': 12})
+        self.assertTrue(e1.match(e2))
+
+    def test_When_matching_wildcards_and_exact_incorrect_values_They_are_not_matched(self):
+        e1 = Event(device='dev', key='*', data='*')
+        e2 = Event(device='dev1', key='key', data={'value': 12})
+        self.assertFalse(e1.match(e2))
+
+    def test_When_matching_list_where_one_event_matches_It_matches(self):
+        e1 = Event(device='dev', key='key', data='data')
+        e2 = Event(device='dev', key='key', data='data2')
+        e3 = Event(device='dev', key='key', data='data')
+        self.assertTrue(e1.match([e2,e3]))
+
+    def test_When_matching_list_where_both_events_match_It_matches(self):
+        e1 = Event(device='dev', key='key', data='data')
+        e2 = Event(device='dev', key='key', data='data')
+        e3 = Event(device='dev', key='key', data='data')
+        self.assertTrue(e1.match([e2,e3]))
+
+    def test_When_matching_list_where_no_events_match_It_does_not_matches(self):
+        e1 = Event(device='dev', key='key', data='data')
+        e2 = Event(device='dev', key='key', data='data1')
+        e3 = Event(device='dev', key='key', data='data2')
+        self.assertFalse(e1.match([e2,e3]))
+        
+    # Matching on event data    
+
+    def test_When_data_matches_exactly_It_is_a_match(self):
+        e1 = Event(device='dev', key='key', data={'value': 2, 'type': 'humidity'}) # occurring
+        e2 = Event(device='dev', key='key', data={'value': 2, 'type': 'humidity'}) # rule
+        self.assertTrue(e1.match(e2))
+
+    def test_When_data_does_not_match_It_is_not_a_match(self):
+        e1 = Event(device='dev', key='key', data={'value': 2, 'type': 'humidity1'}) # occurring
+        e2 = Event(device='dev', key='key', data={'value': 2, 'type': 'humidity2'}) # rule
+        self.assertFalse(e1.match(e2))
+
+    def test_When_data_matches_wildcard_It_is_a_match(self):
+        e1 = Event(device='dev', key='key', data={'value': 2, 'type': 'humidity'}) # occurring
+        e2 = Event(device='dev', key='key', data={'value': 2, 'type': '*'})        # rule
+        self.assertTrue(e1.match(e2))
+
+    def test_When_configured_data_is_subset_of_occuring_data_It_is_a_match(self):
+        e1 = Event(device='dev', key='key', data={'value': 2, 'type': 'humidity'}) # occurring
+        e2 = Event(device='dev', key='key', data={'value': 2})                     # rule
+        self.assertTrue(e1.match(e2))
+
+    def test_When_occurring_data_is_subset_of_configured_data_It_is_not_a_match(self):
+        e1 = Event(device='dev', key='key', data={'value': 2})                      # occurring
+        e2 = Event(device='dev', key='key', data={'value': 2, 'type': 'humidity'})  # rule
+        self.assertFalse(e1.match(e2))
+
+    def test_When_configured_data_is_subset_of_occuring_data_with_wildcard_It_is_a_match(self):
+        e1 = Event(device='dev', key='key', data={'value': 2, 'type': 'humidity'}) # occurring
+        e2 = Event(device='dev', key='key', data={'value': '*'})                   # rule
+        self.assertTrue(e1.match(e2))
+
+    def test_When_occurring_data_is_subset_of_configured_data_with_wildcard_It_is_not_a_match(self):
+        e1 = Event(device='dev', key='key', data={'value': 2})                        # occurring
+        e2 = Event(device='dev', key='key', data={'value': '*', 'type': 'humidity'})  # rule
+        self.assertFalse(e1.match(e2))
+
+    def test_When_deep_data_matches_exactly_It_is_a_match(self):
+        e1 = Event(device='dev', key='key', data={'value': 2, 'extra': {'type': 'humidity'}}) # occurring
+        e2 = Event(device='dev', key='key', data={'value': 2, 'extra': {'type': 'humidity'}}) # rule
+        self.assertTrue(e1.match(e2))
+
+    def test_When_deep_data_matches_wildcard_It_is_a_match(self):
+        e1 = Event(device='dev', key='key', data={'value': 2, 'extra': {'type': 'humidity'}}) # occurring
+        e2 = Event(device='dev', key='key', data={'value': 2, 'extra': {'type': '*'}})        # rule
+        self.assertTrue(e1.match(e2))
+
+    def test_When_deep_configured_data_is_subset_of_occuring_data_It_is_a_match_1(self):
+        e1 = Event(device='dev', key='key', data={'value': 2, 'extra': {'type': 'humidity'}}) # occurring
+        e2 = Event(device='dev', key='key', data={'value': 2})                                # rule
+        self.assertTrue(e1.match(e2))
+
+    # Hm.. Unsure about this one. Should we treat it as a match or not?
+    def test_When_deep_configured_data_is_subset_of_occuring_data_It_is_a_match_2(self):
+        e1 = Event(device='dev', key='key', data={'value': 2, 'extra': {'type': 'humidity'}}) # occurring
+        e2 = Event(device='dev', key='key', data={'value': 2, 'extra': {}})                   # rule
+        self.assertTrue(e1.match(e2))
+
+    def test_When_deep_occurring_data_is_subset_of_configured_data_It_is_not_a_match_1(self):
+        e1 = Event(device='dev', key='key', data={'value': 2})                                 # occurring
+        e2 = Event(device='dev', key='key', data={'value': 2, 'extra': {'type': 'humidity'}})  # rule
+        self.assertFalse(e1.match(e2))
+
+    def test_When_deep_occurring_data_is_subset_of_configured_data_It_is_not_a_match_2(self):
+        e1 = Event(device='dev', key='key', data={'value': 2, 'extra': {}})                    # occurring
+        e2 = Event(device='dev', key='key', data={'value': 2, 'extra': {'type': 'humidity'}})  # rule
+        self.assertFalse(e1.match(e2))
+
