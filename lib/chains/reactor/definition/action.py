a53b1bb3b1d0b3a3ad93ea60d067e3a94a558b68
Stian Strandem <stian@eonbit.com>
2015-05-16 20:14:41 +0000

    Log reactor actions to logfile

--- lib/chains/reactor/definition/action.py
+++ lib/chains/reactor/definition/action.py
@@ -1,7 +1,8 @@
-from chains.common import amqp
+from chains.common import amqp, log
 
 # todo: option for async (ie. no rpc, just push msg to amqp)
 def Action(device, action, params=None):
+    log.info("Run Action: %s.%s( %s )" % (device, action, params))
     if not params:
         params = []
     topic = 'da.%s.%s' % (device, action)
