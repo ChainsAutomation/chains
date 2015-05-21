e518364f11f1cfbd68a2ce6b1f2b95f9ec7b70a3
Stian Strandem <stian@eonbit.com>
2015-05-21 15:55:33 +0000

    Simplified btooth device. Only send presence-<nick> event. The rest can (hopefully) be done in rules.

--- lib/chains/devices/btooth/__init__.py
+++ lib/chains/devices/btooth/__init__.py
@@ -8,18 +8,6 @@ except ImportError, e:
 import time, chains.device
 from chains.common import log
 
-'''
-@todo: describe events
-    * <MAC> - True/False
-    * GuestLeft - <MAC>
-    * UnknownLeft <MAC>
-    * GuestArrive <MAC>
-    * UnknownArrive <MAC>
-    * ALLGuestsGone
-@todo: implement rest of device (actions) - sms, position, obex, etc
-    * See old device in branches/0.2
-'''
-
 class BtoothDevice(chains.device.Device):
 
     def onInit(self):
@@ -40,13 +28,9 @@ class BtoothDevice(chains.device.Device):
 
     def poll(self):
 
-        # Fetch MAC addresses for all nearby devices
         addresses = self.getBluetoothDevices()
         if not addresses:
             return
-
-        presentGuestDeviceCountBefore = self.getPresentGuestDeviceCount()
-        presentUnknownDeviceCountBefore = self.getPresentUnknownDeviceCount()
 
         # And process each of them
         for address in addresses:
@@ -60,14 +44,6 @@ class BtoothDevice(chains.device.Device):
             if self.state[address]['present']:
                 if now > (self.state[address]['time'] + self.goneTime):
                     self.onDeviceLeft(address)
-
-        # If all guests left, fire AllGuestsGone event
-        if presentGuestDeviceCountBefore > 0 and self.getPresentGuestDeviceCount() == 0:
-            self.sendEvent('AllGuestsGone', {'value':True})
-
-        # If all unknowns left, fire AllUnknownsGone event
-        if presentUnknownDeviceCountBefore > 0 and self.getPresentUnknownDeviceCount() == 0:
-            self.sendEvent('AllUnknownsGone', {'value':True})
 
     def onDeviceSeen(self, address):
         isArrival = False
@@ -87,59 +63,49 @@ class BtoothDevice(chains.device.Device):
     def onDeviceArrived(self, address):
 
         # Lookup bluetooth name if not already set
-        if not self.state[address]['name']:
+        if not self.state[address].get('name'):
             try:
                 name = bluetooth.lookup_name(address)
             except:
                 name = 'Unknown'
             self.state[address]['name'] = name
+
         name = self.state[address]['name']
         nick = self.state[address]['nick']
 
-        # Send arrival events
-        isGuest = (address in self.guests)
-        if isGuest:
-            self.sendEvent('GuestArrived',      {'address': address, 'name': name, 'nick': nick})
-            self.sendEvent('Guest-%s' % nick,   {'address': address, 'name': name, 'nick': nick, 'value': True})
-            # Do we really need this?
-            #if self.getPresentGuestDeviceCount() == 1:
-            #    self.sendEvent('FirstGuest',    {'address': address, 'name': name, 'nick': nick})
-        else:
-            self.sendEvent('UnknownArrived',    {'address': address, 'name': name})
-            self.sendEvent('Unknown-%s' % name, {'address': address, 'name': name, 'value': True})
-            # Do we really need this?
-            #if self.getPresentUnknownDeviceCount() == 1:
-            #    self.sendEvent('FirstUnknown',  {'address': address, 'name': name})
+        type = 'unknown'
+        key  = address
+        if address in self.guests:
+            type = 'guest'
+            key  = nick
+
+        self.sendEvent('presence-%s' % key, {
+            'address': address,
+            'name':    name,
+            'nick':    nick,
+            'type':    type,
+            'value':   True
+        })
 
     def onDeviceLeft(self, address):
-        isGuest = (address in self.guests)
         name = self.state[address]['name']
         nick = self.state[address]['nick']
-        if isGuest:
-            self.sendEvent('GuestLeft',         {'address': address, 'name': name, 'nick': nick})
-            self.sendEvent('Guest-%s' % nick,   {'address': address, 'name': name, 'nick': nick, 'value': False})
-        else:
-            self.sendEvent('UnknownLeft',       {'address': address, 'name': name})
-            self.sendEvent('Unknown-%s' % name, {'address': address, 'name': name, 'value': False})
         self.state[address]['present'] = False
         self.state[address]['time'] = time.time()
 
-    def getPresentDeviceCount(self, isGuest=None):
-        n = 0
-        for address in self.state:
-            item = self.state[address]
-            if isGuest in [True,False]:
-                isGuest2 = (address in self.guests)
-                if isGuest2 != isGuest:
-                    continue
-            if item['present']:
-                n += 1
-        return n
+        type = 'unknown'
+        key  = address
+        if address in self.guests:
+            type = 'guest'
+            key  = nick
 
-    def getPresentGuestDeviceCount(self):
-        return self.getPresentDeviceCount(isGuest=True)
-    def getPresentUnknownDeviceCount(self):
-        return self.getPresentDeviceCount(isGuest=False)
+        self.sendEvent('presence-%s' % key, {
+            'address': address,
+            'name':    name,
+            'nick':    nick,
+            'type':    type,
+            'value':   False
+        })
 
     def getBluetoothDevices(self):
         log.debug('Getting bluetooth devices')
