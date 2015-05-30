14b4d0200a79176040f7ca36b1389b8467a43c8b
ChrisQ <chrisq@chainsautomation.com>
2015-05-30 20:26:48 +0200

    testing log function

--- lib/chains/devices/system/__init__.py
+++ lib/chains/devices/system/__init__.py
@@ -11,7 +11,8 @@ class SystemDevice(Device):
 
     def onInit(self):
         self.interval = int(self.config.get('interval')) or 10
-        log.info('SystemDevice interval is %d') % self.interval
+        log.info('SystemDevice interval is: ')
+        log.info(self.interval)
 
     def onStart(self):
         """ start loop """
