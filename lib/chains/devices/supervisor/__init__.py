557031b0ab6ba71397bbb4e310331768e26313e8
ChrisQ <chrisq@chainsautomation.com>
2015-06-05 22:40:44 +0200

    supervisor device functionality in place

--- lib/chains/devices/supervisor/__init__.py
+++ lib/chains/devices/supervisor/__init__.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-import sys
 import time
 from chains.device import Device
 from chains.common import log
@@ -12,6 +11,7 @@ from . import supervisor as sprv
 class SupervisorDevice(Device):
 
     def onInit(self):
+        log('Starting Supervisor device')
         self.interval = self.config.getInt('interval') or 300  # seconds
         self.server = xmlrpclib.ServerProxy(
             'http://127.0.0.1',
@@ -20,52 +20,114 @@ class SupervisorDevice(Device):
                 'unix:///var/run/supervisor.sock'
             )
         )
-        print self.server.supervisor.getState()
 
     def onStart(self):
         while not self._shutdown:
             cur_state = self._get_state()
+            self._send_server_state()
             self._send_state(cur_state)
             time.sleep(self.interval)
 
     def _send_state(self, progs):
         for program in progs:
-            self.sendEvent(program,
-                           {
-                               'key': 'val',
-                           })
+            self.sendEvent(program, progs[program])
+
+    def _send_server_state(self):
+        srv_state = self._get_server_state(self.server)
+        if srv_state:
+            self.sendEvent('server_state', {'value': srv_state['statename']})
+        else:
+            self.sendEvent('error', {'type': 'server_state', 'program': 'supervisor'})
 
     def _get_state(self):
         # Fetch current state from supervisor
-        pass
+        return sprv.state(self.server)
 
     def _get_server_state(self):
         # Fetch current server state from supervisor
-        pass
+        return sprv.server_state(self.server)
 
-    def action_stop(self, program):
-        pass
+    def action_server_state(self, program):
+        srv_state = self._get_server_state(self.server)
+        if srv_state:
+            self.sendEvent('server_state', {'value': srv_state['statename']})
+        else:
+            self.sendEvent('error', {'type': 'server_state', 'program': 'supervisor'})
 
-    def action_stop_all(self, program):
-        pass
 
-    def action_stop_group(self, group):
-        pass
+    def action_server_shutdown(self):
+        if sprv.server_shutdown(self.server):
+            self.sendEvent('server_state', {'value': 'SHUTDOWN'})
+        else:
+            self.sendEvent('error', {'type': 'server_shutdown', 'program': 'supervisor'})
+
+    def action_server_restart(self):
+        if sprv.server_restart(self.server):
+            self.sendEvent('server_state', {'value': 'RESTARTING'})
+        else:
+            self.sendEvent('error', {'type': 'server_restart', 'program': 'supervisor'})
+
+    def action_program_info(self, program):
+        info = sprv.info(self.server, program)
+        if info:
+            self.sendEvent(program, info)
+        else:
+            self.sendEvent('error', {'type': 'info', 'program': program})
 
     def action_start(self, program):
-        pass
+        start = sprv.start(self.server, program)
+        if start:
+            self.sendEvent('start', {'value': program})
+        else:
+            self.sendEvent('error', {'type': 'start', 'program': program})
 
-    def action_start_all(self, program):
-        pass
+    def action_start_all(self):
+        if sprv.start_all(self.server):
+            self.sendEvent('start_all', {'program': 'supervisor'})
+        else:
+            self.sendEvent('error', {'type': 'start_all', 'program': 'supervisor'})
 
     def action_start_group(self, group):
-        pass
+        start = sprv.start_group(self.server, group)
+        if start:
+            self.sendEvent('start_group', {'value': group})
+        else:
+            self.sendEvent('error', {'type': 'start_group', 'group': group})
+
+    def action_stop(self, program):
+        stop = sprv.stop(self.server, program)
+        if stop:
+            self.sendEvent('stop', {'value': program})
+        else:
+            self.sendEvent('error', {'type': 'stop', 'program': program})
+
+    def action_stop_all(self):
+        stop = sprv.stop_all(self.server)
+        if stop:
+            self.sendEvent('stop_all', {'program': 'supervisor'})
+        else:
+            self.sendEvent('error', {'type': 'stop_all', 'program': 'supervisor'})
+
+    def action_stop_group(self, group):
+        stop = sprv.stop_group(self.server, group)
+        if stop:
+            self.sendEvent('stop_group', {'value': group})
+        else:
+            self.sendEvent('error', {'type': 'stop_group', 'group': group})
 
     def action_restart(self, program):
-        pass
+        restart = sprv.restart(self.server, program)
+        if restart:
+            self.sendEvent('restart', {'program': program})
+        else:
+            self.sendEvent('error', {'type': 'restart', 'program': program})
 
     def action_restart_group(self, group):
-        pass
+        restart = sprv.restart_group(self.server, group)
+        if restart:
+            self.sendEvent('restart', {'group': group})
+        else:
+            self.sendEvent('error', {'type': 'restart', 'group': group})
 
     def action_state(self, program):
         pass
--- lib/chains/devices/supervisor/supervisor.py
+++ lib/chains/devices/supervisor/supervisor.py
@@ -1,48 +1,84 @@
 def server_state(server):
     cur_state = server.supervisor.getState()
-    print cur_state
+    if cur_state:
+        return {'state': cur_state['statename']}
+    return False
 
 
-def server_shutdown():
-    pass
+def server_shutdown(server):
+    if server.supervisor.shutdown():
+        return True
+    return False
 
 
-def server_restart():
-    pass
+def server_restart(server):
+    if server.supervisor.restart():
+        return True
+    return False
+
+
+def info(server, prog):
+    cur_state = server.supervisor.getProcessInfo(prog)
+    if cur_state:
+        return {prog: cur_state}
+    return False
 
 
 def state(server):
-    cur_state = server.supervisor.getState()
-    print cur_state
+    all_state = server.supervisor.getAllProcessInfo()
+    ret = {}
+    if all_state:
+        for one in all_state:
+            ret.update({one['name']: one})
+    return ret
 
 
-def stop(program):
-    pass
+def start(server, prog):
+    stat = server.supervisor.startProcess(prog)
+    return stat
 
 
-def stop_all(program):
-    pass
+def start_all(server, program):
+    stat = server.supervisor.startAllProcesses(prog)
+    return stat
 
 
-def stop_group(group):
-    pass
+def start_group(server, group):
+    stat = server.supervisor.startProcessGroup(group)
+    return stat
 
 
-def start(program):
-    pass
+def stop(server, prog):
+    stat = server.supervisor.stopProcess(prog)
+    return stat
 
 
-def start_all(program):
-    pass
+def stop_group(server, group):
+    stat = server.supervisor.stopProcessGroup(group)
+    return stat
 
 
-def start_group(group):
-    pass
+def stop_all(server):
+    stat = server.supervisor.stopAllProcesses()
+    return stat
 
 
-def restart(program):
-    pass
+def restart(server, prog):
+    return stop(prog) and start(prog)
 
 
-def restart_group(group):
-    pass
+def restart_group(server, group):
+    return stop_group(group) and start_group(group)
+
+if __name__ == '__main__':
+    import supervisor.xmlrpc
+    import xmlrpclib
+    server = xmlrpclib.ServerProxy(
+        'http://127.0.0.1',
+        transport=supervisor.xmlrpc.SupervisorTransport(
+            None, None,
+            'unix:///var/run/supervisor.sock'
+        )
+    )
+    print server_state()
+    print state()
