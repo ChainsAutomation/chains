d0bd6c316d0f4755be8c91be817296ffa40dfb88
ChrisQ <chrisq@chainsautomation.com>
2015-06-08 15:27:38 +0200

    Starting philips sicp definitions

---
+++ lib/chains/devices/phitv/sicp.py
@@ -0,0 +1,6 @@
+CMD = {
+    0xA2: {'desc': 'Platform and Version Labels - Get',
+        0x00: 'Get SICP implementation version',
+        0x01: 'Get the software label and version information of the platform ',
+    }
+}
