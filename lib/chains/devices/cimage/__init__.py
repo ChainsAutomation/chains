8ede6cf5ae825a6832ac568cce10f2722d04550c
chrisaq <chris@kotelett.no>
2015-07-03 16:52:12 +0200

    Start of CImage device

---
+++ lib/chains/devices/cimage/__init__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python2
+
+                self.sendEvent(line[1:4], {'value': line[4:]})
+import sys
+import time
+
+from chains.device import Device
+from chains.common import log
+import serial
+from . import cimage
+
+
+class CImageDevice(Device):
+
+    def onInit(self):
+        log('CV image init.')
+
+# TODO: Decide:
+# * Pure action device, handling existing images?
+# * Keep watching a cam for matching and such?
+
+#    def onStart(self):
+#        log('CV Image starting.')
+#        while not self._shutdown:
+#            # TODO: sleep probably not needed?
+#            time.sleep(0.1)
+
