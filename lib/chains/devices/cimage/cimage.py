530729305be3ea98495b81bdbf1ea179a0a9b1d4
chrisaq <chris@kotelett.no>
2015-07-05 13:56:27 +0200

    OpenCV stuff on hold until I can get it working locally

--- lib/chains/devices/cimage/cimage.py
+++ lib/chains/devices/cimage/cimage.py
@@ -3,14 +3,25 @@
 import time
 import numpy as np
 import cv2
+import urllib2
+
+def _read_image(path):
+    """ Read image from filesystem or url, return local path if ok (?) """
+    if path.startswith('http'):
+        # TODO: download image and store? Read and discard?
+        pass
+    else:
+        # expect image path to be a local file
+        pass
 
 if __name__ == '__main__':
     from pprint import pprint
     import sys
     import serial
-    act = sys.argv[1]
-    img = sys.argv[2]
+    in_img = sys.argv[1]
+    act = sys.argv[2]
     # cmds = sys.argv[3:]
     # for c in cmds:
     #     print "command : %s" % c
     #     time.sleep(0.5)
+    img = cv2.imread(in_img,0)
