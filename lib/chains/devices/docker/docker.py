d5baf196265b864a785008dc5c4e3d85577e91d0
chrisaq <chris@kotelett.no>
2015-08-07 19:47:13 +0200

    Considering requests_unixsocket

--- lib/chains/devices/docker/docker.py
+++ lib/chains/devices/docker/docker.py
@@ -3,6 +3,12 @@ import socket
 import json
 import time
 
+# import requests_unixsocket
+# import requests
+# requests_unixsocket.monkeypatch()
+# r = requests.get('http+unix://%2Fvar%2Frun%2Fdocker.sock/containers/')
+
+
 def pretty(jobj):
     print json.dumps(jobj, sort_keys=True, indent=4, separators=(',', ': '))
 
