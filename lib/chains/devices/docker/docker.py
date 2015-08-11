613ad45c6f72ff90e144d2cb89f760b392be5a2c
chrisaq <chris@kotelett.no>
2015-08-11 21:19:12 +0200

    Get detailed image information from socket

--- lib/chains/devices/docker/docker.py
+++ lib/chains/devices/docker/docker.py
@@ -12,6 +12,25 @@ import time
 def pretty(jobj):
     print json.dumps(jobj, sort_keys=True, indent=4, separators=(',', ': '))
 
+def container(cid):
+    print "def container(cid)"
+    cid = json.loads(cid)
+    pretty(cid)
+    if u'status' in cid:
+        docker2 = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        docker2.connect("/var/run/docker.sock")
+        message = 'GET /containers/%s/json HTTP/1.1\n\n' % cid['id']
+        print message
+        docker2.send(message)
+        info = docker2.recv(1024)
+        print "container info:"
+        print info
+        print "container info ends"
+        docker2.close()
+    else:
+        print "couldn't find status in cid"
+    print "def container(cid) ends"
+
 def reader():
     docker = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     docker.connect("/var/run/docker.sock")
