c84ab89babc8b92a948809c9f53fb9e2113a8372
chrisaq <chris@kotelett.no>
2015-08-17 21:51:40 +0200

    docker class

--- lib/chains/devices/docker/docker.py
+++ lib/chains/devices/docker/docker.py
@@ -9,62 +9,65 @@ import time
 # r = requests.get('http+unix://%2Fvar%2Frun%2Fdocker.sock/containers/')
 
 
-def pretty(jobj):
-    print json.dumps(jobj, sort_keys=True, indent=4, separators=(',', ': '))
+class Docker(object):
 
-def container(cid):
-    print "def container(cid)"
-    cid = json.loads(cid)
-    pretty(cid)
-    if u'status' in cid:
-        docker2 = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        docker2.connect("/var/run/docker.sock")
-        message = 'GET /containers/%s/json HTTP/1.1\n\n' % cid['id']
-        print message
-        docker2.send(message)
-        info = docker2.recv(1024)
-        print "container info:"
-        print info
-        print "container info ends"
-        docker2.close()
-    else:
-        print "couldn't find status in cid"
-    print "def container(cid) ends"
+    def pretty(self, jobj):
+        print json.dumps(jobj, sort_keys=True, indent=4, separators=(',', ': '))
 
-def reader():
-    docker = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    docker.connect("/var/run/docker.sock")
-    message = 'GET /events HTTP/1.1\n\n'
-    # message = 'GET /events?since=1374067924'
-    docker.send(message)
-    # socat - UNIX-CONNECT:/var/run/docker.sock
-    # GET /events HTTP/1.1
-    while True:
-        data = docker.recv(1024)
-        if not data:
-            break
+    def container(self, cid):
+        print "def container(cid)"
+        cid = json.loads(cid)
+        pretty(cid)
+        if u'status' in cid:
+            docker2 = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+            docker2.connect("/var/run/docker.sock")
+            message = 'GET /containers/%s/json HTTP/1.1\n\n' % cid['id']
+            print message
+            docker2.send(message)
+            info = docker2.recv(1024)
+            print "container info:"
+            print info
+            print "container info ends"
+            docker2.close()
         else:
-            print '%s' % data
-            for part in parts:
-                if part.startswith('{'):
-                    container(part)
-            #try:
-            #    print "json - " + str(json.loads(part))
-            #    if type(json.loads(part)) == int:
-            #        print "status message ignoring"
-            #    else:
-            #        container(part)
-            #except ValueError, e:
-            #    print "non-json content: %s" % part
-        if "QUIT" == data:
-            break
+            print "couldn't find status in cid"
+        print "def container(cid) ends"
 
-    print "Shutting down..."
-    docker.close()
-    print "Done"
+    def reader(self):
+        docker = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        docker.connect("/var/run/docker.sock")
+        message = 'GET /events HTTP/1.1\n\n'
+        # message = 'GET /events?since=1374067924'
+        docker.send(message)
+        # socat - UNIX-CONNECT:/var/run/docker.sock
+        # GET /events HTTP/1.1
+        while True:
+            data = docker.recv(1024)
+            if not data:
+                break
+            else:
+                print '%s' % data
+                for part in parts:
+                    if part.startswith('{'):
+                        container(part)
+                #try:
+                #    print "json - " + str(json.loads(part))
+                #    if type(json.loads(part)) == int:
+                #        print "status message ignoring"
+                #    else:
+                #        container(part)
+                #except ValueError, e:
+                #    print "non-json content: %s" % part
+            if "QUIT" == data:
+                break
+
+        print "Shutting down..."
+        docker.close()
+        print "Done"
 
 
 if __name__ != '__main__':
     pass
 else:
     print "trying to read from docker socket"
+    Docker.reader()
