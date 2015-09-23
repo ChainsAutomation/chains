a6f3b30e9f79b13dedb1630f108d90df356cbf86
Stian Strandem <stian@eonbit.com>
2015-09-23 17:41:42 +0000

    Added orchestrator action to chains command line

---
+++ lib/chains/commandline/commands/OrchestratorAction.py
@@ -0,0 +1,19 @@
+from chains.commandline.commands import Command
+from chains.commandline import InvalidParameterException
+from chains.common import amqp
+
+class CommandOrchestratorAction(Command):
+    def main(self, *args):
+        """
+        Send action to orchestrator
+
+        @param action   string  Action to send
+        @param args     mixed   Arguments to pass to action
+        @optional args
+        """
+        args = list(args)
+        try:
+            action = args.pop(0)
+        except IndexError:
+            raise InvalidParameterException('usage: orchestrator action <action> [args]')
+        return self.rpc('%s%s.%s.%s' % (amqp.PREFIX_ORCHESTRATOR, amqp.PREFIX_ACTION, 'main', action), args)
