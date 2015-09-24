b37dd5217392dfcd92c2dcb13e7c3ecb270e4d67
Stian Strandem <stian@eonbit.com>
2015-09-24 19:27:06 +0000

    Work on base for webgui

--- misc/webgui/gulpfile.js
+++ misc/webgui/gulpfile.js
@@ -3,13 +3,17 @@ var gulp = require('gulp'), 
     notify = require("gulp-notify"),
     bower = require('gulp-bower'),
 	minifycss = require('gulp-minify-css'),
-	rename = require('gulp-rename');
-
+	rename = require('gulp-rename'),
+    fileinclude = require('gulp-file-include'),
+    rimraf = require('gulp-rimraf'),
+    concat = require('gulp-concat');
 
 var config = {
-    sassDir: './app/sass',
+    sassDir:  './app/sass',
+    jsDir:    './app/js',
     bowerDir: './bower_components',
-    distDir: './public'
+    distDir:  './public',
+    htmlDir:  './app/html'
 }
 
 gulp.task('bower', function() { 
@@ -23,7 +27,7 @@ gulp.task('icons', function() { 
 });
 
 gulp.task('css', function() { 
-	return sass(config.sassDir + '/style.scss', {
+	return sass(config.sassDir + '/app.scss', {
         style: 'compressed',
         loadPath: [
           config.sassDir,
@@ -38,9 +42,69 @@ gulp.task('css', function() { 
 	.pipe(gulp.dest(config.distDir + '/css'));
 });
 
-// Rerun the task when a file changes
-gulp.task('watch', function() {
-     gulp.watch(config.sassPath + '/**/*.scss', ['css']); 
+gulp.task('js:app', function() {
+
+    // App
+
+    return gulp.src([
+        config.jsDir + '/**/*.js'
+    ])
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest(config.distDir + '/js'));
+
 });
 
-gulp.task('default', ['bower', 'icons', 'css']);
+gulp.task('js:vendor', function() {
+
+    // Vendor
+
+    return gulp.src([
+        config.bowerDir + '/jquery/dist/*.min.*',
+        config.bowerDir + '/html5shiv/dist/html5shiv.min.js',
+        config.bowerDir + '/respond/dest/respond.min.js',
+        config.bowerDir + '/bootstrap/dist/js/*.min.*',
+        config.bowerDir + '/knockout/dist/knockout.js',
+        config.bowerDir + '/routie/dist/routie.min.js',
+        config.bowerDir + '/moment/min/*.min.js',
+        config.bowerDir + '/moment-timezone/moment-timezone.js'
+    ])
+    .pipe(gulp.dest(config.distDir + '/js'));
+
+});
+
+gulp.task('js', ['js:app','js:vendor'], function(){ });
+
+gulp.task('html', function() {
+    gulp.src([
+        config.htmlDir + '/index.html'
+    ])
+    .pipe(fileinclude({
+        prefix: '@@',
+        basepath: '@file'
+    }))
+    .pipe(gulp.dest(config.distDir));
+});
+
+gulp.task('clean', function () {
+  return gulp.src([
+    config.distDir + '/{js,css,fonts}/**/*',
+    config.distDir + '/index.html'
+  ])
+  .pipe(rimraf());
+});
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+    gulp.watch(config.sassPath + '/**/*.scss', ['css']); 
+    gulp.watch(config.jsDir + '/**/*.js', ['js']);
+    gulp.watch(config.htmlDir + '/*/**.html', ['html']);
+});
+
+gulp.task('default', ['clean'], function(){
+    gulp.run('bower');
+    gulp.run('icons');
+    gulp.run('css');
+    gulp.run('js:vendor');
+    gulp.run('js:app');
+    gulp.run('html');
+});
