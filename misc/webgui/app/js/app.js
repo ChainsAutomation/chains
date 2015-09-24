5efae1f1366db0265ebe7c2b8df05d41aa8b217c
Stian Strandem <stian@eonbit.com>
2015-09-24 19:48:56 +0000

    Simple proof-of-concept webgui that shows services with status

--- misc/webgui/app/js/app.js
+++ misc/webgui/app/js/app.js
@@ -1,6 +1,6 @@
 window.Chains = {};
 window.Chains.Backend = {};
-window.Chains.Devices = {};
+window.Chains.Services = {};
 
 window.Chains.Backend.call = function(method, url, data, callback) {
 
@@ -30,10 +30,28 @@ window.Chains.Backend.post = function(url, data) {
     window.Chains.Backend.call('POST', url, data);
 }
 
-window.Chains.Device.list = function() {
-
-    window.Chains.Backend.get('/devices', function(result) {
-        console.log('devices result:', result);
-    });
-
+window.Chains.Services.list = function(callback) {
+    window.Chains.Backend.get('/services', callback);
 };
+
+$(document).ready(function(){
+    console.log('test...');
+    window.Chains.Services.list(function(result){
+
+        var e = $('#services');
+        for(id in result) {
+            var srv = result[id];
+            if (!srv.id)
+                continue;
+            e.append(
+                '<tr>' +
+                '<td>' + srv.name + '</td>' +
+                '<td class="service-' + (srv.online ? 'online' : 'offline') + '">' + 
+                (srv.online ? 'Online' : 'Offline') + 
+                '</td>' +
+                '</tr>'
+            );
+        }
+
+    });
+});
