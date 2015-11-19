0c3d68f5786e4129fd1d207aa168ad2999c910c9
Ole-Kenneth Bratholt <ok@rait.no>
2015-11-19 09:08:30 +0100

    Fix typo

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-if [ -z "$DOCKER_EMAIL"]; then
+if [ -z "$DOCKER_EMAIL" ]; then
     echo "Not logging in to Docker Hub"
 else
     docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
