22b27b115d91b344116ed0f53deee8cd553f828f
Ole-Kenneth Bratholt <ok@rait.no>
2015-11-21 15:09:54 +0100

    Add docker autobuilder script

---
+++ bin/docker-autobuilder.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+TYPE="$1"
+REMOVE_VERSION=`git rev-parse origin/master`
+LOCAL_VERSION=`git rev-parse HEAD`
+OS=$(uname -m)
+
+if [ -z $TYPE ]; then
+    TYPE="slave";
+fi
+
+if [ $OS = "_x86_64" ]; then
+    IMAGE="chains";
+else
+    IMAGE="rpi-chains";
+fi
+
+while true; do
+    git fetch
+    if [ $REMOVE_VERSION != $LOCAL_VERSION ]; then
+        git pull
+        make test
+        if [ "$?" -eq 0 ]; then
+            ./bin/dockerfile-assemble.py $TYPE
+            docker build --no-cache -t chains/$IMAGE-$TYPE . && docker push chains/$IMAGE-$TYPE
+        else
+            sleep 3600 # Tests failing waiting at least one hour before trying again
+        fi
+    fi;
+    sleep 60;
+done;
