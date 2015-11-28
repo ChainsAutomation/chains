4265c1d214252d90b5a08c754461e0ec738f9d80
Stian Strandem <stian@eonbit.com>
2015-11-28 17:41:49 +0000

    Add device to SendEvent stmt in rules

--- lib/chains/reactor/definition/sendevent.py
+++ lib/chains/reactor/definition/sendevent.py
@@ -1,11 +1,12 @@
 from chains.common import amqp
 
-def SendEvent(service, key, data):
+def SendEvent(service, key, data, device=None):
     topic = '%s%s.%s.%s' % (amqp.PREFIX_SERVICE, amqp.PREFIX_EVENT, service, key)
     connection = amqp.Connection()
     producer = connection.producer(queueName='reactor-sendevent')
     event = {
         'service': service,
+        'device': device,
         'key':    key,
         'data':   data
     }
