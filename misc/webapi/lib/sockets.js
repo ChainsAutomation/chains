f1f26c95e70b7699a1f1c660e07ee0d7ad810d12
Stian Strandem <stian@eonbit.com>
2015-11-28 18:20:15 +0000

    Emit events like "service-event" rather than "se.tellstick.change" from webapi to webgui, so it is easier to subscribe to a bunch of events

--- misc/webapi/lib/sockets.js
+++ misc/webapi/lib/sockets.js
@@ -14,11 +14,66 @@ module.exports = function(httpServer) {
 		self.enableDebug = value;
 	}
 
-	self.send = function(topic, data) {
+	self.send = function(chainsTopic, data) {
+
+        var tmp = chainsTopic.split('.');
+        var prefix = tmp[0];
+        var ioTopic = null;
+
+        // todo: we may not want to push all of these to webgui
+        // should add a system for saying what we want to subscribe to
+        switch (prefix) {
+            case 'se':
+                ioTopic = 'service-event';
+                break;
+            case 'sa':
+                ioTopic = 'service-action';
+                break;
+            case 'sr':
+                ioTopic = 'service-action-response';
+                break;
+            case 'sh':
+                ioTopic = 'service-heartbeat';
+                break;
+            case 'me':
+                ioTopic = 'manager-event';
+                break;
+            case 'ma':
+                ioTopic = 'manager-action';
+                break;
+            case 'mr':
+                ioTopic = 'manager-action-response';
+                break;
+            case 'mh':
+                ioTopic = 'manager-heartbeat';
+                break;
+            case 're':
+                ioTopic = 'reactor-event';
+                break;
+            case 'ra':
+                ioTopic = 'reactor-action';
+                break;
+            case 'rr':
+                ioTopic = 'reactor-action-response';
+                break;
+            case 'rh':
+                ioTopic = 'reactor-heartbeat';
+                break;
+            case 'H':
+                ioTopic = 'hearbeat-request';
+                break;
+        }
+
+        if (!ioTopic)
+            return;
+
+        data['topic'] = chainsTopic;
+
 		for(var i=0; i < self.sockets.length; i++) {
-			self.sockets[i].debug('send: ' + topic, data);
-			self.sockets[i].emit(topic, data);
+			self.sockets[i].debug('send: ' + ioTopic, data);
+			self.sockets[i].emit(ioTopic, data);
 		}
+
 	}
 
 	self.io.on('connection', function(socket) {
