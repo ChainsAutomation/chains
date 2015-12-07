09ec39063c128bd81416be2c80ae54add2359026
Ole-Kenneth Bratholt <ok@rait.no>
2015-12-07 21:53:27 +0100

    Using requirejs to load js files

--- misc/webgui/app/js/app.js
+++ misc/webgui/app/js/app.js
@@ -1,119 +1,125 @@
-window.Chains = window.Chains || {};
+define(['jquery', 'knockout', 'routie', 'socket.io', 'bootstrap', 'moment', 'moment-timezone'], function($, ko, routie, io) {
+    window.ko = ko;
+    window.Chains = window.Chains || {};
 
-window.Chains.App = function() {
+    var App = function() {
 
-    var self = this;
+        var self = this;
 
-    window.Chains.instance = self;
+        window.Chains.instance = self;
 
-    self.config = {
-        restUrl:   '/api',
-        socketUrl: 'http://' + window.location.hostname + ':7890'
-    };
+        self.config = {
+            restUrl:   '/api',
+            socketUrl: 'http://' + window.location.hostname + ':7890'
+        };
 
-    self.router = routie;
-    self.backend = null; // set in start()
-    self.socket = null; // set in start()
+        self.router = routie;
+        self.backend = null; // set in start()
+        self.socket = null; // set in start()
 
-    self.isSocketConnected = ko.observable(false);
+        self.isSocketConnected = ko.observable(false);
 
-    self.services = new window.Chains.Services(self);
-    self.managers = new window.Chains.Managers(self);
-    self.reactors = new window.Chains.Reactors(self);
-    self.state    = new window.Chains.State(self);
+        self.services = new window.Chains.Services(self);
+        self.managers = new window.Chains.Managers(self);
+        self.reactors = new window.Chains.Reactors(self);
+        self.state    = new window.Chains.State(self);
 
-    self.views          = {};
-    self.views.services = new window.Chains.View.Services(self);
-    self.views.system   = new window.Chains.View.System(self);
-    self.views.state    = new window.Chains.View.State(self);
-    self.views.devices  = new window.Chains.View.Devices(self);
+        self.views          = {};
+        self.views.services = new window.Chains.View.Services(self);
+        self.views.system   = new window.Chains.View.System(self);
+        self.views.state    = new window.Chains.View.State(self);
+        self.views.devices  = new window.Chains.View.Devices(self);
 
-    self.configure = function(key, data) {
-        if (self.config[key] === undefined) {
-            console.error('No such configurable key: ' + key);
-            return;
-        }
-        self.config[key] = data;
-    };
+        self.configure = function(key, data) {
+            if (self.config[key] === undefined) {
+                console.error('No such configurable key: ' + key);
+                return;
+            }
+            self.config[key] = data;
+        };
 
-    self.setView = function(view) {
-        $('.view').hide();
-        $('#view-' + view).show();
-        $('body').removeClass().addClass('active-view-' + view);
-        if (self.views[view] && self.views[view].resume)
-            self.views[view].resume();
-    };
+        self.setView = function(view) {
+            $('.view').hide();
+            $('#view-' + view).show();
+            $('body').removeClass().addClass('active-view-' + view);
+            if (self.views[view] && self.views[view].resume)
+                self.views[view].resume();
+        };
 
-    self.navbar = ko.observableArray([
-        { url: '#/devices', name: 'Devices' },
-        { url: '#/services', name: 'Services' },
-        { url: '#/system', name: 'System' },
-        { url: '#/state', name: 'State' },
-    ]);
+        self.navbar = ko.observableArray([
+            { url: '#/devices', name: 'Devices' },
+            { url: '#/services', name: 'Services' },
+            { url: '#/system', name: 'System' },
+            { url: '#/state', name: 'State' },
+        ]);
 
-    self.start = function() {
+        self.start = function() {
 
-        self.backend = new window.Chains.Backend(self.config.restUrl);
-        self.socket = io.connect(self.config.socketUrl);
+            self.backend = new window.Chains.Backend(self.config.restUrl);
+            self.socket = io.connect(self.config.socketUrl);
 
-        ko.applyBindings(self);
-        self.setView('index');
+            ko.applyBindings(self);
+            self.setView('index');
 
-        // Init routes
+            // Init routes
 
-        self.router('/', function(){ self.setView('index'); });
-        self.router('/devices', function(){ self.setView('devices'); });
-        self.router('/services', function(){ self.setView('services'); });
-        self.router('/system', function(){ self.setView('system'); });
-        self.router('/state', function(){ self.setView('state'); });
+            self.router('/', function(){ self.setView('index'); });
+            self.router('/devices', function(){ self.setView('devices'); });
+            self.router('/services', function(){ self.setView('services'); });
+            self.router('/system', function(){ self.setView('system'); });
+            self.router('/state', function(){ self.setView('state'); });
 
-        // Load backend data
+            // Load backend data
 
-        self.services.load(function(){
-            self.managers.load(function(){
-                self.reactors.load(function(){
-                    self.state.load(function(){
-                        console.log('Loaded...');
+            self.services.load(function(){
+                self.managers.load(function(){
+                    self.reactors.load(function(){
+                        self.state.load(function(){
+                            console.log('Loaded...');
+                        });
                     });
                 });
             });
-        });
 
-        // Socket events
+            // Socket events
 
-        self.socket.on('sa.web.reload', function() {
-            console.log('reloading');
-            document.location.reload();
-        });
+            self.socket.on('sa.web.reload', function() {
+                console.log('reloading');
+                document.location.reload();
+            });
 
-        self.socket.on('service-event', function(data) {
+            self.socket.on('service-event', function(data) {
 
-            var attrs = {};
+                var attrs = {};
 
-            for(var key in data) {
-                if (key == 'service' || key == 'device' || key == 'data' || key == 'key')
-                    continue;
-                attrs[key] = data[key];
-            }
+                for(var key in data) {
+                    if (key == 'service' || key == 'device' || key == 'data' || key == 'key')
+                        continue;
+                    attrs[key] = data[key];
+                }
 
-            self.state.set(
-                data.service,
-                data.device,
-                data.data,
-                attrs
-            );
+                self.state.set(
+                    data.service,
+                    data.device,
+                    data.data,
+                    attrs
+                );
 
-        });
+            });
 
-        self.socket.on('service-heartbeat', function(data) {
-            // if (data == 2) service is still online
-        });
+            self.socket.on('service-heartbeat', function(data) {
+                // if (data == 2) service is still online
+            });
 
-        // Socket connection status
+            // Socket connection status
 
-        setInterval(function(){
-            self.isSocketConnected(self.socket.socket.connected);
-        }, 2000);
+            setInterval(function(){
+                self.isSocketConnected(self.socket.socket.connected);
+            }, 2000);
+        };
     };
 
-};
+    window.Chains.App = App;
+
+    return App;
+});
--- misc/webgui/app/startup.js
+++ misc/webgui/app/startup.js
@@ -1,19 +1,32 @@
-$(document).ready(function(){
+require.config({
+    paths: {
+        jquery: 'jquery.min',
+        bootstrap: 'bootstrap.min',
+        routie: 'routie.min',
+        moment: 'moment.min',
+        'socket.io': '/api/socket.io/socket.io'
+    },
 
-    var app = new window.Chains.App();
+    shim: {
+        routie: {
+            exports: 'routie'
+        },
+        bootstrap: {
+            deps: ['jquery']
+        }
+    }
+});
+requirejs(['app'], function(App) {
+    //This function is called when scripts/helper/util.js is loaded.
+    //If util.js calls define(), then this function is not fired until
+    //util's dependencies have loaded, and the util argument will hold
+    //the module value for "helper/util".
+
+    var app = new App();
 
     // Example - configure different backend url
     // app.configure('restUrl', 'http://webapi.my-chains.com');
     // app.configure('socketUrl', 'http://socketio.my-chains.com');
 
     app.start();
-
 });
-/*
-requirejs([""], function(util) {
-    //This function is called when scripts/helper/util.js is loaded.
-    //If util.js calls define(), then this function is not fired until
-    //util's dependencies have loaded, and the util argument will hold
-    //the module value for "helper/util".
-});
-*/
