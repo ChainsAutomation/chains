3cf3aceb6791603e97881325196c9c3acf26cf02
ChrisQ <chris@kotelett.no>
2016-02-21 20:41:47 +0100

    utf-8 all the json messages

--- lib/chains/common/amqp.py
+++ lib/chains/common/amqp.py
@@ -234,6 +234,7 @@ class Producer(Channel):
             msg = amqp.Message(
                 json.encode(message),
                 content_type='text/json'
+                content_encoding=encoding,
             )
         self.ch.basic_publish(msg, self.exchange, key)
         return correlationId
