993488d7ab4157941ceec9533238e31eb00d73d1
ChrisQ <chris@kotelett.no>
2016-02-21 21:47:39 +0100

    pep8

--- lib/chains/reactor/worker/context.py
+++ lib/chains/reactor/worker/context.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
+
 class Context:
     def __init__(self, state):
         self.state = state
--- lib/chains/reactor/worker/ruleinstances.py
+++ lib/chains/reactor/worker/ruleinstances.py
@@ -4,29 +4,29 @@ from chains.common import log
 from chains.reactor.worker.rulerunner import RuleRunner
 
 # RuleInstances
-# 
+#
 # List of instances for a single rule
 # F.ex. if rule A has maxCount=2, then the RuleInstances for rule A
 # has a list of 2 instances of RuleRunner for rule A.
-# 
+#
 # Starts out with 0 instances and each time the first event in rule
 # is matched, it spawns a new runner, until maxCount is reached.
-# 
-# All incoming events are passed to all active RuleRunner instances.
 #
+# All incoming events are passed to all active RuleRunner instances.
+
+
 class RuleInstances:
 
     def __init__(self, id, rule, maxCount, context):
 
-        self.id       = id
+        self.id = id
         self.maxCount = maxCount  # number of paralell instances allowed
-        self.rule     = rule      # the rule definition (generator) itself
-        self.runners  = []        # active rule runners
-        self.context  = context
-        self.event    = next(rule.rule(None)) # need first event to know when to spawn runners
+        self.rule = rule      # the rule definition (generator) itself
+        self.runners = []        # active rule runners
+        self.context = context
+        self.event = next(rule.rule(None))  # need first event to know when to spawn runners
 
         self.debug('Init RuleInstances')
-
 
     def onEvent(self, event):
 
@@ -59,11 +59,10 @@ class RuleInstances:
             log.debug(msg, data)
         else:
             log.debug(msg)
-    
+
     def info(self, msg, data=None):
         msg = "RuleInstances: #%s: %s" % (self.id, msg)
         if data:
             log.info(msg, data)
         else:
             log.info(msg)
-    
