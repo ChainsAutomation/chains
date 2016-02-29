35449cfe8aeb664c8eaecdd736abd4275cb36c40
ChrisQ <chris@kotelett.no>
2016-02-29 16:15:54 +0100

    debug decorator tests for py3

--- lib/chains/common/debug.py
+++ lib/chains/common/debug.py
@@ -1,21 +1,23 @@
-# py3: Done
 from __future__ import print_function
 from __future__ import absolute_import
 from functools import wraps
 import inspect
 from six.moves import map
+import sys
 
-def debug(func):
+def debug_decorator(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
+        PY3 = sys.version_info[0] == 3
+        if PY3:
         # inspect.signature only available in py3
-        # print('debug: ' + func.__name__ + str(inspect.signature(func)))
+            print('debug: ' + func.__name__ + str(inspect.signature(func)))
         args_str = "(" + ", ".join(map(str, args)) + ", " + str(kwargs) + ")"
         print("debug: " + func.__name__ + args_str)
         return func(*args, **kwargs)
     return wrapper
 
-@debug
+@debug_decorator
 def _test(arg1, arg2, kw1='something', kw2='stuff'):
     print('This is all I do')
 
