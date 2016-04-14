6c7d7b735da16eac4e3cec6a8ee73102138a576f
Stian Strandem <stian@eonbit.com>
2016-04-14 18:30:40 +0000

    Adjust chains amqp event command to optionally take data as json

--- lib/chains/commandline/commands/AmqpEvent.py
+++ lib/chains/commandline/commands/AmqpEvent.py
@@ -2,12 +2,15 @@ from __future__ import absolute_import
 from __future__ import print_function
 from chains.commandline.commands import Command
 from chains.common import amqp
-import json
+import json, types
 
 class CommandAmqpEvent(Command):
     def main(self, serviceId, device, key, data):
         """ Inject an event on message bus """
-        if data[0] == '{':
+
+        if type(data) == types.DictType:
+            data = data
+        elif data[0] == '{':
             data = json.loads(data)
         else:
             data = {'test': {'value': data}}
