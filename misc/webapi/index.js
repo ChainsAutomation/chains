8f2a7353821522f9f0660a5439439228d10f045e
Stian Strandem <stian@eonbit.com>
2017-11-10 16:15:28 +0000

    Allow CORS * in webapi

--- misc/webapi/index.js
+++ misc/webapi/index.js
@@ -96,7 +96,15 @@ if (enableDebug)
 
 // Express - Setup
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+};
+
 app.use(expressBodyParser.json());
+app.use(allowCrossDomain);
 
 // Express - Routes
 
