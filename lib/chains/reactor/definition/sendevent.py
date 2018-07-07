048adf36d74e980d48fbe860cf891e67f8ff4e4b
Stian Strandem <stian@eonbit.com>
2018-07-07 13:28:13 +0000

    Do not require all params in SendEvent

--- lib/chains/reactor/definition/sendevent.py
+++ lib/chains/reactor/definition/sendevent.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 from chains.common import amqp
 
-def SendEvent(service, key, data, device=None):
+def SendEvent(service, key='nokey', data=None, device=None):
     topic = '%s%s.%s.%s' % (amqp.PREFIX_SERVICE, amqp.PREFIX_EVENT, service, key)
     connection = amqp.Connection()
     producer = connection.producer(queueName='reactor-sendevent')
